ba0735fd85d33b2d4766f53d8a89d5f9913532a3
eguinaldo <eguinaldocoura@gmail.com>
2025-04-11 19:24:15 -0300

    init rep

---
+++ bilheteria.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define "plateia.txt"
+
+void mainMenu(){
+    printf("-------------------------------\n");
+    printf("MENU\n");
+    printf("-------------------------------\n");
+    printf("0-Sair do programa\n");
+    printf("1-Mostrar plateia\n");
+    printf("2-Mostrar ocupacao\n");
+    printf("3-Vender ingresso\n");
+    printf("4-Cancelar ingresso (devolucao)\n");
+    printf("-------------------------------\n");
+    printf("Escolha uma opcao:");
+}
+
+void mostrarPlateia(char* plateia){
+
+    File 
+
+    printf("PLATEIA\n");
+    printf("-------------------------------\n");
+
+
+    for(int i = 1 ; i <= 10 ; i++){
+        for (int j = 1 ; j <= 12; j++){
+            lugares[i][j];
+        }
+    }
+
+    printf("-------------------------------\n");
+    printf("FIM DA PLATEIA\n");
+
+}
+
+void mostrarOcupacao(){
+
+}
+
+void venderIngresso(){
+
+}
+
+void cancelarIngresso(){
+
+}
+
+void sairDoPrograma(){
+
+}
+
+void menuProcess(){
+
+
+}
+
+void inputProcess(){
+
+}
+
+void inicializarArquivo(){
+
+}
+
+
+
+int main(int arc, char* arv){
+
+
+
+
+
+
+
+    return 1;
+}
